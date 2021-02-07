c2c4a07832ae3f3535fadad731e2067cda71b7d7
LTiseanu <ltiseanu@yahoo.com>
2021-02-07 14:59:15 +0200

    Setting Attributes

--- src/components/counterComp.jsx
+++ src/components/counterComp.jsx
@@ -3,23 +3,27 @@ import React, { Component } from 'react';
 class CounterComp extends Component {
     state = { // here we have all the props
         hello: "Hellooo",
-        count: 0
+        count: 0,
+        imageUrl: "https://picsum.photos/200"
     }
     render() {  
         return (
             <React.Fragment>
-                <h1>{this.state.hello} {2 + 2}</h1>
-                <button>Increment</button>
-                {this.formatCount()}
+                <span className="badge badge-primary m-2">{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
             </React.Fragment>
         );
     }
 
+    // <h1>{this.state.hello} {2 + 2}</h1>
+    // <img className="hidden" src={this.state.imageUrl} alt=""/>
+    // {this.formatCount()}
     formatCount() {
+        // object destructuring
         const {count} = this.state;
         const x = <h1>JSX expression here</h1>
-	    return count === 0 ? <h1>Zero</h1> : count;
-        // or: return count === 0 ? "Zero" : count ;
+        return count === 0 ? "Zero" : count;
+        // or: return count === 0 ? <h1>Zero</h1> : count;
     }
 }
  
