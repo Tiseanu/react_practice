1d3127fc01076935056fd5f87db85befa5fe49ae
LTiseanu <ltiseanu@yahoo.com>
2021-02-07 13:41:33 +0200

    adding the counterComp component

---
+++ src/components/counterComp.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class CounterComp extends Component {
+    state = {  }
+    render() {  
+        return <h1>Hello hi</h1>;
+    }
+}
+ 
+export default CounterComp;
+
