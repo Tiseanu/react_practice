c7eecc79d2cff996f5c7bfce38cc7590f8cd9164
LTiseanu <ltiseanu@yahoo.com>
2021-02-07 14:39:35 +0200

    Adding dynamic value

--- src/components/counterComp.jsx
+++ src/components/counterComp.jsx
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 
 class CounterComp extends Component {
-    state = {  }
+    state = { // here we have all the props
+        hello: "Hellooo",
+        count: 0
+    }
     render() {  
         return (
             <React.Fragment>
-                <h1>Hello</h1>
+                <h1>{this.state.hello} {2 + 2}</h1>
                 <button>Increment</button>
+                {this.formatCount()}
             </React.Fragment>
         );
+    }
+
+    formatCount() {
+        const {count} = this.state;
+        const x = <h1>JSX expression here</h1>
+	    return count === 0 ? <h1>Zero</h1> : count;
+        // or: return count === 0 ? "Zero" : count ;
     }
 }
  
