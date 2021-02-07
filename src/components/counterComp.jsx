12492a1bbb716ca4ade1b56d821ee9b23d80ea94
LTiseanu <ltiseanu@yahoo.com>
2021-02-07 15:55:43 +0200

    Adding Conditional rendering

--- src/components/counterComp.jsx
+++ src/components/counterComp.jsx
@@ -4,27 +4,39 @@ class CounterComp extends Component {
     state = { // here we have all the props
         hello: "Hellooo",
         count: 1,
-        imageUrl: "https://picsum.photos/200"
+        imageUrl: "https://picsum.photos/200",
+        tags: ['tag1', 'tag2', 'tag3'],
     };
 
-    /*{this.style}
+    renderTags() {
+        // if (this.state.tags.length === 0) return null
+        if (this.state.tags.length === 0) return <p>No tags</p>
+        return <ul> { this.state.tags.map(tag => <li key={tag}>{ tag }</li>) }</ul>;
+    }
+
+    render() { 
+        return (
+            <React.Fragment>
+                { this.state.tags.length === 0 && "Add some tags" }
+                {this.renderTags()}
+            </React.Fragment>
+        );
+    }
+
+    /*
+    <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button style={{fontSize: 13}} className="btn btn-secondary btn-sm">Increment</button>
+    */
+
+     /*{this.style}
     style = {
         // camel case notation:
         fontSize: 10, // 10px implicit
         fontWeight: "bold"
     };*/
 
-    render() { 
-        return (
-            <React.Fragment>
-                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button style={{fontSize: 13}} className="btn btn-secondary btn-sm">Increment</button>
-            </React.Fragment>
-        );
-    }
-
     // Ctrl+Shift+R
-    getBadgeClasses() {
+    getBadgeClasses() { // rendering classes
         let classes = "badge m2 badge-";
         classes += (this.state.count === 0) ? "warning" : "primary";
         return classes;
